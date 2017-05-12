b81b3bdf99126133d1cbcefdf1e4828870496826
dragos.boghitoiu <dragos.boghitoiu@mulesoft.com>
2017-05-12 15:57:29 +0300

    * renamed main package * updated support_info * added validation for HOST & PORT params * refactored test directories by following certification guidelines * removed "Optional" annotation from host && port * created SystemTestSuite

---
+++ src/main/java/org/mule/modules/cassandradb/utils/ColumnSerializer.java
@@ -0,0 +1,76 @@
+/**
+ * (c) 2003-2017 MuleSoft, Inc. The software in this package is published under the terms of the Commercial Free Software license V.1 a copy of which has been included with this distribution in the LICENSE.md file.
+ */
+package org.mule.modules.cassandradb.utils;
+
+/**
+ * Pair of values used to serialize columns
+ */
+public class ColumnSerializer {
+
+    /**
+     * The column name that needs to be serialized
+     */
+    String key;
+
+    /**
+     * The type of serializer that will be used to encode the data retrieved from Cassandra DB
+     */
+    String type;
+
+    /**
+     * Default Constructor
+     */
+    public ColumnSerializer() {
+        key = null;
+        type = null;
+    }
+
+    /**
+     * Parametrized Constructor
+     *
+     * @param key  The column name that needs to be serialized
+     * @param type The type of serializer that will be used to encode the data retrieved from Cassandra DB
+     */
+    public ColumnSerializer(String key, String type) {
+        this.key = key;
+        this.type = type;
+    }
+
+    /**
+     * Retrieves the key
+     *
+     * @return the result is a key
+     */
+    public String getKey() {
+        return key;
+    }
+
+    /**
+     * Sets key
+     *
+     * @param key The column name as key.
+     */
+    public void setKey(String key) {
+        this.key = key;
+    }
+
+    /**
+     * Retrieves type
+     *
+     * @return the result is a type of serializer.
+     */
+    public String getType() {
+        return type;
+    }
+
+
+    /**
+     * Sets type
+     *
+     * @param type The type of serializer that will be used to encode the data
+     */
+    public void setType(String type) {
+        this.type = type;
+    }
+}
