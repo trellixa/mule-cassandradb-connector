8f38170ea2fa092ef9c6a64ca050147a5eb9b63b
Alejandro Mataloni <amataloni@gmail.com>
2017-11-28 11:00:22 -0300

    CASS-37 | Starting migration to mule4

---
+++ src/main/java/org/mule/modules/cassandradb/internal/extension/CassandraDBExtension.java
@@ -0,0 +1,12 @@
+/**
+ * (c) 2003-2016 MuleSoft, Inc. The software in this package is
+ * published under the terms of the Commercial Free Software license V.1, a copy of which
+ * has been included with this distribution in the LICENSE.md file.
+ */
+package org.mule.modules.cassandradb.internal.extension;
+
+import org.mule.runtime.extension.api.annotation.Extension;
+
+@Extension(name = "CassandraDB")
+public class CassandraDBExtension {
+}
